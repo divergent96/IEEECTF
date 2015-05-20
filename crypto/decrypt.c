8c8c5a6cbd23d2a98ae86de6cfc8b0c490d668f2
divergent96 <susheelpatel.96@gmail.com>
2015-05-21 00:56:22 +0530

    made new repo

---
+++ crypto/decrypt.c
@@ -0,0 +1,33 @@
+//Not working,segmentation fault
+
+#include<stdio.h>
+#include<string.h>
+
+main()
+  {
+    char str1[100],str2[100],str3[100],temp[100];
+    char ans[100];
+    
+    printf("Input the string : ");
+    scanf(" %s",str1);
+    printf("\nEnter key : ");
+    scanf(" %s",str2);
+    
+    int n=strlen(str1);
+    int i,j,l=0,k;
+    for(j=0;j,n/3;j++){
+    for(i=l,k=0;i<l+3;i++,k++){
+        temp[k]=str1[i];
+        }
+    str3[j]=atoi(temp);
+    l=l+3;
+    str3[j]=str3[j]^str2[j];
+    }
+    
+    printf("\n %s",str3);
+
+
+    
+     printf("\n##Counter=%d##\n",i);
+        
+  }
