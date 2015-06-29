be052fe67d4a656bcd4cc778dcc113e82c57ae39
divergent96 <susheelpatel.96@gmail.com>
2015-06-29 17:55:25 +0530

    The code is working properly now.

--- cryptopals/set1/chal3.py
+++ cryptopals/set1/chal3.py
@@ -10,8 +10,10 @@ similarityarr = dict()
 
 for key in xrange(0,256):
     sample = xorstr(msg,chr(key))
-    similarityarr[key] = similarity(charfreq.values(),Counter(sample).values())
-        
-best = max(similarityarr.iteritems())
+    similarityarr[key] = similarity(charfreq.values(),frequency(sample).values())
 
+best = max(similarityarr.iteritems(),key = operator.itemgetter(1))[0]#Seems this function uses operator.itemgetter(1) fetches the value from the array at key row and column 1 and feeds it to iteritems which is used in max 
+#For more reference see link http://stackoverflow.com/questions/268272/getting-key-with-maximum-value-in-dictionary
+
+print best
 print xorstr(msg,chr(best))
